9d2e49a5d3db429622a0b2453debcab1b15b733d
nshroff1000 <neelshroff1000@gmail.com>
2019-12-07 20:31:55 -0500

    Pushed basic frontend

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,23 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
 
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+  	next();
+});
+
 app.listen(3001, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3001!');
 });
 
 
- var index = require('./routes/index');
+var index = require('./routes/index');
 app.use(index);
+
 
 process.on('SIGINT', function() {
 server.close(function(err) {
@@ -15,15 +25,3 @@ if (err) winston.error(err);
 process.exit(0);
 });
 });
-// var app = express();
-
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-// // uncomment after placing your favicon in /public
-// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
-// app.listen('8081', function() {
-//   console.log('Server running on port 8081');
-// });
--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,73 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Table, Divider, Tag } from 'antd';
+import { Tabs } from 'antd';
 import './App.css';
+import 'antd/dist/antd.css';
+
+const { TabPane } = Tabs;
+
+
+class StockTable extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {table_data: ""};
+
+    this.columns = [
+      {
+        title: "Stock Name",
+        key: 'STOCK_NAME',
+        dataIndex: 'STOCK_NAME'
+      },
+      {
+        title: "Stock Ticker",
+        key: 'STOCK_TICKER',
+        dataIndex: 'STOCK_TICKER'
+      }
+    ];
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3001/stocks")
+    .then(response => response.json())
+    .then(data => {
+      this.setState({table_data: data});
+      console.log(data);
+    })
+  }
+
+  render() {
+    if (this.table_data == "") {
+      return "";
+    } else {
+      return <Table rowKey="STOCK_ID" dataSource={this.state.table_data} columns={this.columns}/>;
+    }
+  }
+
+}
+
+function Title(props) {
+  return <h1>Finding Stock Trends</h1>;
+}
+
+const element = <Title/>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Set up Basic Front End
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {element}
+      <Tabs defaultActiveKey="1">
+        <TabPane tab="All Stocks" key="1">
+          <StockTable/>
+        </TabPane>
+        <TabPane tab="Stock Prices" key="2">
+          Content of Tab Pane 2
+        </TabPane>
+        <TabPane tab="Tab 3" key="3">
+          Content of Tab Pane 3
+        </TabPane>
+      </Tabs>,
     </div>
   );
 }
